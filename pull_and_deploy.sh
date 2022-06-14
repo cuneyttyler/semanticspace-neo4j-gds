645ca86578c0a1afa2a52208c5d0254a751dde16
Cuneyt Tyler <cuneyttyler@gmail.com>
2022-06-14 16:52:57 +0300

    2.1.1

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -1,5 +1,5 @@
 git pull origin test
 cd build/distributions
-sudo cp semantic-space-gds-2.0.4.jar /var/lib/neo4j/plugins
+sudo cp semantic-space-gds-1.0.1.jar /var/lib/neo4j/plugins
 sudo systemctl restart neo4j.service
 tail -f /var/log/neo4j/debug.log
